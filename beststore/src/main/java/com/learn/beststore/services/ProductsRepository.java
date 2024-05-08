244b3ce445a62b1c32a37f69b60681ac92b43616
Dang Gia Loc <hahagioi169@gmail.com>
2024-05-09 00:02:06 +0700

    Learn SpringMVC - Create Product page and create product API - Done

--- beststore/src/main/java/com/learn/beststore/services/ProductsRepository.java
+++ beststore/src/main/java/com/learn/beststore/services/ProductsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.learn.beststore.models.Product;
 
 public interface ProductsRepository extends JpaRepository<Product, Integer> {
-
+	
 }
